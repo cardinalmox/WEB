3af66ea53fcdf12f66abf2c5647b0a162167f0c8
cardinalmox <nailmukhamedgaliev2003@mail.ru>
2024-03-08 19:50:53 +0300

    Started task 11

--- Lab_1/code/index.php
+++ Lab_1/code/index.php
@@ -25,3 +25,10 @@ echo "\n" , $number1 + $number3;
 $last_month = 1187.23;
 $this_month = 1089.98;
 echo "\n Разница предыдущего и этого месяца : " , $last_month - $this_month;
+
+//task 11
+$num_languages = 4;
+$months = 11;
+$days = $months * 16;
+$days_per_language = $days / $num_languages;
+echo "days per language: {$days_per_language}\n";
