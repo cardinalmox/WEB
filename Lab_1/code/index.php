e26d776a5989ef1c71443db435480ff33d59a558
cardinalmox <nailmukhamedgaliev2003@mail.ru>
2024-03-08 19:05:03 +0300

    Completed task1

--- Lab_1/code/index.php
+++ Lab_1/code/index.php
@@ -4,6 +4,8 @@ echo phpinfo();
 //task 1
 $very_bad_unclear_name = "15 chicken wings";
 
+$order = & $very_bad_unclear_name;
+$order .= ",cola and one hamburger";
 
 echo "\nYour order is: $very_bad_unclear_name.";
 
