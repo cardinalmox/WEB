5ac5324f9500806eb0b949942fdf38cea37e3cbf
cardinalmox <nailmukhamedgaliev2003@mail.ru>
2024-03-08 20:10:25 +0300

    Complete task 13

--- Lab_1/code/index.php
+++ Lab_1/code/index.php
@@ -37,3 +37,12 @@ echo "days per language: {$days_per_language}\n";
 echo 8**2, "\n";
 
 //task 13
+$my_num = 8;
+$answer = $my_num;
+$answer += 2;
+$answer *= 2;
+$answer -= 2;
+$answer /= 2;
+$answer -= $my_num;
+echo "\n" , $answer;
+echo "\n";
