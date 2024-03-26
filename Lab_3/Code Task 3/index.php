d2ea793709601d0029f932605a86648f586096e8
cardinalmox <nailmukhamedgaliev2003@mail.ru>
2024-03-26 22:32:00 +0300

    Complete Task 3

--- Lab_3/Code Task 3/index.php
+++ Lab_3/Code Task 3/index.php
@@ -42,3 +42,28 @@
             <th>Title</th>
             <th>Description</th>
             </thead>
+            <?php
+            $categories = ['cars', 'others', 'real estate'];
+            foreach ($categories as $category) {
+                $directory = "./categories/$category";
+                if (is_dir($directory)) {
+                    $files = scandir($directory);
+                    foreach ($files as $file) {
+                        if ($file != '.' && $file != '..') {
+                            $content = file_get_contents("$directory/$file");
+                            $values = explode("\n", $content);
+                            echo "<tbody>";
+                            echo "<td>{$values[0]}</td>";
+                            echo "<td>$category</td>";
+                            echo "<td>{$values[1]}</td>";
+                            echo "<td>{$values[2]}</td>";
+                            echo "</tbody>";
+                        }
+                    }
+                }
+            }
+            ?>
+        </table>
+    </div>
+</body>
+</html>
